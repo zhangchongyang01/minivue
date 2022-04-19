8e5af6bbcd4efec204ca9c9e98d9732d8b605b03
zhangchongyang01 <1247629239@qq.com>
2022-04-20 07:33:27 +0800

    8.实现effect的scheduler功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,6 @@
 class ReactiveEffect{
   private _fn:any;
-  constructor(fn){
+  constructor(fn, public scheduler?){
     this._fn = fn;
   }
   run(){
@@ -28,13 +28,17 @@ export function trigger(target,key){
   let depsMap = targetMap.get(target);
   let dep = depsMap.get(key);
   for(const effect of dep){
-    effect.run();
+    if(effect.scheduler){
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
   }
 }
 
 let activeEffect;
-export function effect(fn){
-  const _effect = new ReactiveEffect(fn);
+export function effect(fn, options: any= {}){
+  const _effect = new ReactiveEffect(fn,options.scheduler);
   _effect.run();
   return _effect.run.bind(_effect)
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -14,7 +14,7 @@ describe('effect', () => {
     user.age++;
     expect(nextAge).toBe(12)
   });
-  it('', () => {
+  it('should return runner when call effect', () => {
     // 1.effect(fn) -> function (runner) -> fn -> return 
     let foo = 10;
     const runner = effect(() => {
@@ -26,5 +26,30 @@ describe('effect', () => {
     expect(foo).toBe(12);
     expect(r).toBe("foo");
 
+  })
+  it("scheduler", () => {
+    let dummy;
+    let run:any;
+    const scheduler = jest.fn(()=> {
+      run = runner;
+    });
+    const obj = reactive({foo:1});
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      {scheduler}
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    // should be called on first trigger
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    // should not run yet
+    expect(dummy).toBe(1);
+    // manually run
+    run();
+    // should have run
+    expect(dummy).toBe(2);
   })
 })
